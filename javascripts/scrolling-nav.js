115d4ba024cca91529f36f7d7c2158d808eab6c3
james89 <jrothpearl@gmail.com>
2014-06-12 16:03:28 -0400

    cleaning script tags

--- javascripts/scrolling-nav.js
+++ javascripts/scrolling-nav.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 
+
     $('#nav').onePageNav();
 
-// navbar default should have below class
-$(".navbar-fixed-top").addClass("navbar-big");
+    // navbar default should have below class
+    $(".navbar-fixed-top").addClass("navbar-big");
 
 
 
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    var width = $(window).width()
+    //collapse the navbar on scroll, and resize navbar height
+    $(window).scroll(function() {
+        var width = $(window).width()
 
-    // if ( ($(".navbar").offset().top > 30) && (width < 767) ) {
-    //     $(".navbar-fixed-top").addClass("top-nav-collapse");
-    // } else {
-    //     $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    // }
+    if ( ($(".navbar").offset().top > 30) && (width < 767) ) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
 
-    // this will only add the additional padding if: window width is greater than 767, and
+    //this will only add the additional padding if: window width is greater than 767, and
     if ( (width > 767) && ($(".navbar").offset().top > 50) ) {
        $(".navbar-fixed-top").removeClass("navbar-big");
     } else {
