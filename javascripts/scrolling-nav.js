bcbb4f89c306ab72793b49f9eb6bb4733cf41746
james89 <jrothpearl@gmail.com>
2014-06-12 15:42:08 -0400

    rewriting scrolling-nav javascript, to include css transition class on scroll

--- javascripts/scrolling-nav.js
+++ javascripts/scrolling-nav.js
@@ -1,9 +1,24 @@
+
+// navbar default should have below class
+$(".navbar-fixed-top").addClass("navbar-big");
+
+
+
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 30) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    var width = $(window).width()
+
+    // if ( ($(".navbar").offset().top > 30) && (width < 767) ) {
+    //     $(".navbar-fixed-top").addClass("top-nav-collapse");
+    // } else {
+    //     $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    // }
+
+    // this will only add the additional padding if: window width is greater than 767, and
+    if ( (width > 767) && ($(".navbar").offset().top > 50) ) {
+       $(".navbar-fixed-top").removeClass("navbar-big");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("navbar-big");
     }
 });
 
